e78862ee5cc429873697297b2d83c83e942c3662
aironalonso <eronjoswaalonso@gmail.com>
2022-04-26 15:39:36 +0800

    we-build-proj

--- src/components/Developers.js
+++ src/components/Developers.js
@@ -1,9 +1,32 @@
-import { Box } from '@mui/material';
+import { Avatar, Box, Stack } from '@mui/material';
 import React from 'react';
 
 const Developers = () => {
   return (
-    <Box sx={{ backgroundColor: '#3c055e', height: 625 }}></Box>
+    <Box sx={{ backgroundColor: '#3c055e', height: 625 }}>
+      <Stack direction='row' spacing={14} sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 12,
+      }}>
+        <Avatar 
+        alt='Kiel Paderes'
+        src='https://placeimg.com/640/480/people'
+        sx={{ height: 180, width: 180 }}
+        />
+        <Avatar 
+        alt='Airon Alonso'
+        src='https://placeimg.com/640/480/people'
+        sx={{ height: 180, width: 180 }}
+        />
+        <Avatar 
+        alt='Loraine Ostan'
+        src='https://placeimg.com/640/480/people'
+        sx={{ height: 180, width: 180 }}
+        />
+      </Stack>
+    </Box>
   );
 };
 
